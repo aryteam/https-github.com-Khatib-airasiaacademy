ddcde65a236368982d6988ea22a6916496375e5d
aryteam <69624038+aryteam@users.noreply.github.com>
2023-03-07 14:05:28 +0800

    Add files via upload

--- Advertising.py
+++ Advertising.py
@@ -2,5 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+pd.read_csv('Advertising.csv')
 st.header("My first Streamlit App")
-st.write(pd.DataFrame(Advertising.csv))
+st.write(pd.DataFrame(pd))
