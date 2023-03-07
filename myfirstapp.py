f58ee5079aa645f6eb0d94b05bcdf062779369c8
aryteam <69624038+aryteam@users.noreply.github.com>
2023-03-07 12:01:24 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -23,8 +23,8 @@ elif option=='map':
     st.map(map_data)
 
 else:
-st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
-show = st.checkbox('I agree the terms and conditions')
+    st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+    show = st.checkbox('I agree the terms and conditions')
 
 if show:
     st.write(pd.DataFrame({
