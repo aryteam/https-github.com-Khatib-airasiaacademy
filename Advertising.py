b6d182c8b00cca6c3d00f2edf0cfe427fce6cb2e
aryteam <69624038+aryteam@users.noreply.github.com>
2023-03-07 14:56:26 +0800

    Update Advertising.py

--- Advertising.py
+++ Advertising.py
@@ -6,21 +6,44 @@ df = pd.read_csv('Advertising.csv')
 st.header("My first Streamlit App")
 st.write(df)
 
-option = st.sidebar.selectbox(
-    'Select a mini project',
-     ['line chart','map','T n C','Long Process'])
+st.sidebar.header('User Input Parameters')
 
-if option=='line chart':
-    chart_data = pd.DataFrame(
-    df)
+def user_input_features():
+    sepal_length = st.sidebar.slider('Sales', 4.3, 7.9, 5.4)
+    sepal_width = st.sidebar.slider('TV', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Radio', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Newspaper', 0.1, 2.5, 0.2)
+    data = {'sepal_length': sepal_length,
+            'sepal_width': sepal_width,
+            'petal_length': petal_length,
+            'petal_width': petal_width}
+    features = pd.DataFrame(df, index=[0])
+    return features
 
-    st.line_chart(chart_data)
+df = user_input_features()
 
-elif option=='map':
-    map_data = pd.DataFrame(df,
-    columns=['Sales', 'TV'])
+st.subheader('User Input parameters')
+st.write(df)
 
-    st.map(map_data)
+iris = datasets.load_iris()
+X = iris.data
+Y = iris.target
+
+clf = RandomForestClassifier()
+clf.fit(X, Y)
+
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
+
+st.subheader('Class labels and their corresponding index number')
+st.write(iris.target_names)
+
+st.subheader('Prediction')
+st.write(iris.target_names[prediction])
+#st.write(prediction)
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
 
 
 
