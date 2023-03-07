cdec298a6cea32dee7f2d3c517bcd748992a4da9
aryteam <69624038+aryteam@users.noreply.github.com>
2023-03-07 14:35:58 +0800

    Update Advertising.py

--- Advertising.py
+++ Advertising.py
@@ -5,3 +5,22 @@ import pandas as pd
 df = pd.read_csv('Advertising.csv')
 st.header("My first Streamlit App")
 st.write(df)
+
+option = st.sidebar.selectbox(
+    'Select a mini project',
+     ['line chart','map','T n C','Long Process'])
+
+if option=='line chart':
+    chart_data = pd.DataFrame(
+    df)
+
+    st.line_chart(chart_data)
+
+elif option=='map':
+    map_data = pd.DataFrame(df,
+    columns=['lat', 'lon'])
+
+    st.map(map_data)
+
+        }))
+
